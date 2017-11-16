2b549a8d4bfddaa22786f867d8ba5927a0525e35
Jesus Garcia <jgarcia@seamgen.com>
2017-11-15 21:31:14 -0800

    initialize dependency injection

--- app/src/App.ts
+++ app/src/App.ts
@@ -1,11 +1,13 @@
 import {SimpleDI} from './di/SimpleDI';
+import {IFibonacciRepository} from './api/utils/IFibonacciRepository';
+import {FibonacciRepository} from './api/utils/FibonacciRepository';
 
 const DI = SimpleDI.Instance;
 
 export class App{
     
     public Initialize(){
-        
+        DI.register<IFibonacciRepository>("IFibonacciRepository",new FibonacciRepository());
     }
     
     public Start() : void{
